d2fc7dd1282c34164fe7d428033edad0e7d92d74
LeongLRF <980159737@qq.com>
2019-09-21 14:27:06 +0800

    条件构造器

--- backend/partymanager/src/main/java/com/wyu/partymanager/entity/sys/Role.java
+++ backend/partymanager/src/main/java/com/wyu/partymanager/entity/sys/Role.java
@@ -1,5 +1,7 @@
 package com.wyu.partymanager.entity.sys;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.wyu.partymanager.entity.TimestampEntity;
 import lombok.Data;
 
@@ -10,10 +12,14 @@ import java.sql.Timestamp;
  * @date 2019/9/21 13:23
  */
 @Data
+@TableName("sys_role")
 public class Role implements TimestampEntity {
 
+    @TableField("id")
+    long id;
     String name;
+    String permission;
     Timestamp insertedAt;
     Timestamp updatedAt;
-    String permission;
+
 }
