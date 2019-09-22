aff9d989f8412b06def0831ca9477e93d5c1a30e
LeongLRF <980159737@qq.com>
2019-09-23 00:22:20 +0800

    解决跨域访问

--- backend/partymanager/src/main/java/com/wyu/partymanager/PartymanagerApplication.java
+++ backend/partymanager/src/main/java/com/wyu/partymanager/PartymanagerApplication.java
@@ -3,10 +3,12 @@ package com.wyu.partymanager;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.wyu.partymanager.configuration.LoginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
@@ -17,19 +19,23 @@ public class PartymanagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PartymanagerApplication.class, args);
     }
+
     @Bean//使用@Bean注入fastJsonHttpMessageConvert
     public HttpMessageConverters fastJsonHttpMessageConverters(){
-        //1.需要定义一个Convert转换消息的对象
         FastJsonHttpMessageConverter fastConverter=new FastJsonHttpMessageConverter();
-        //2.添加fastjson的配置信息，比如是否要格式化返回的json数据
-//
         FastJsonConfig fastJsonConfig=new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        //3.在convert中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
-
         HttpMessageConverter converter=fastConverter;
         return new HttpMessageConverters(converter);
+    }
+
+    @Bean
+    public FilterRegistrationBean<LoginFilter> registerFilter(){
+        FilterRegistrationBean<LoginFilter> bean = new FilterRegistrationBean<>();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new LoginFilter());
+        return bean;
     }
 
 }
