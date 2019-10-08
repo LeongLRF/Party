9761d76c8b6f1543a490a8dae828a2bc5caea318
Leong <980159737@qq.com>
2019-10-08 16:49:17 +0800

    各种mapper

---
+++ backend/partymanager/src/main/java/com/wyu/partymanager/mapper/ActivityMapper.java
@@ -0,0 +1,7 @@
+package com.wyu.partymanager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyu.partymanager.entity.sys.Activity;
+
+public interface ActivityMapper extends BaseMapper<Activity> {
+}
---
+++ backend/partymanager/src/main/java/com/wyu/partymanager/mapper/TakePartMapper.java
@@ -0,0 +1,7 @@
+package com.wyu.partymanager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyu.partymanager.entity.sys.TakePart;
+
+public interface TakePartMapper extends BaseMapper<TakePart> {
+}
