dc63d854de304ee5a671f10b9dace39078fff939
Leong <980159737@qq.com>
2019-10-17 17:41:05 +0800

    clazz

--- backend/partymanager/src/main/java/com/wyu/partymanager/entity/sys/Clazz.java
+++ backend/partymanager/src/main/java/com/wyu/partymanager/entity/sys/Clazz.java
@@ -1,11 +1,16 @@
 package com.wyu.partymanager.entity.sys;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.wyu.partymanager.entity.JsonEntity;
+import com.wyu.partymanager.utils.IFilter;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @ApiModel
@@ -14,5 +19,31 @@ public class Clazz implements JsonEntity {
 
     @TableId(value = "id",type = IdType.AUTO)
     long id;
+
+    @ApiModelProperty("班级号")
+    @TableField("code")
+    String code;
+
+    @ApiModelProperty("编辑名称")
+    @TableField("name")
+    String name;
+
+    @ApiModelProperty("班级专业")
+    @TableField("major")
+    String major;
+
+    @ApiModelProperty("班导师")
+    @TableField("teacher")
+    String teacher;
+
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    public static class Filter extends IFilter<Clazz>{
+
+        @Override
+        public Wrapper<Clazz> apply() {
+            return this.queryWrapper();
+        }
+    }
 
 }
