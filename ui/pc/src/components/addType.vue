2c423a5a92978b6584161431ade1d5eb8f287fac
Leong <980159737@qq.com>
2019-10-23 18:13:00 +0800

    activityType

---
+++ ui/pc/src/components/addType.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <a-modal :title="edit ? '修改活动类型' : '添加活动类型'" :visible="visible" @ok="handleOk" @cancel="handleCancel">
+      <span>活动类别:</span>
+      <a-input style="width: 20%"></a-input>
+      <span>活动主题:</span>
+      <a-select
+        mode="tags"
+        placeholder="Please select"
+        v-model="defaultValue"
+        style="width: 160px"
+        :open="false"
+        @popupScroll="popupScroll"
+      >
+      </a-select>
+      <a-button @click="add">添加</a-button>
+    </a-modal>
+    <a-modal title="请输入主题" @ok="handleAdd" @cancel="handleCan" :visible="visible2">
+      <span>主题名称:</span>
+      <a-input style="width: 30%" v-model="name"></a-input>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addType',
+  data () {
+    return {
+      visible: false,
+      edit: false,
+      type: [{
+        name: '',
+        details: []
+      }],
+      visible2: false,
+      defaultValue: [],
+      name: ''
+    }
+  },
+  methods: {
+    handleOk () {
+      this.visible = false
+    },
+    handleCancel () {
+      this.visible = false
+    },
+    open (data, edit) {
+      this.visible = true
+    },
+    handleAdd () {
+      this.defaultValue.push(this.name)
+      this.visible2 = false
+      console.log(this.defaultValue)
+    },
+    handleCan () {
+      this.visible2 = false
+    },
+    add () {
+      console.log('点击了')
+      this.visible2 = true
+    },
+    popupScroll () {
+      console.log('popupScroll')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- ui/pc/src/views/activityType.vue
+++ ui/pc/src/views/activityType.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <a-table :dateSource="dataSource"  :columns="columns"></a-table>
+    <div class="nav">
+      <a-button type='primary' @click="open">
+        <a-icon type="plus" />新增</a-button>
+    </div>
+    <a-table :dateSource="dataSource"  :columns="columns" style="width: 30%;" size="small"></a-table>
+    <add-type ref="addType"></add-type>
   </div>
 </template>
 
 <script>
 export default {
   name: 'activityType',
+  components: {
+    addType: () => import('@/components/addType')
+  },
   data () {
     return {
       columns: [
-        {title: 'name', dataIndex: 'name', key: 'name', align: 'center', width: '50px'},
-        {title: 'details', dataIndex: 'details', key: 'details', align: 'center', width: '150px'},
-        {title: 'operation', key: 'operation', scopedSlots: {customRender: 'operation'}}
+        {title: '名称', dataIndex: 'name', key: 'name', align: 'center', width: '50px'},
+        {title: '主题', dataIndex: 'details', key: 'details', align: 'center', width: '150px'},
+        {title: '操作', key: 'operation', scopedSlots: {customRender: 'operation'}}
       ]
+    }
+  },
+  methods: {
+    open () {
+      this.$refs.addType.open()
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .nav {
+    background-color: #fff;
+    padding: 10px;
+    box-shadow: 3px 3px 3px #ccc;
+    border-radius: 5px;
+    margin-bottom: 20px;
+  }
+</style>
