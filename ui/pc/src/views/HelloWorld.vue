294975b8d2bc91b0267887eefc5fbc43ea2dc240
SziFeng <1046897327@qq.com>
2019-10-24 21:55:04 +0800

    11

--- ui/pc/src/views/HelloWorld.vue
+++ ui/pc/src/views/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height:100%;background:#fff">
-    <h1>~~欢迎来到党员管理系统~~</h1>
+    <h1>欢迎您，现在时间是：<span style="color: #e2151f">{{time}}</span></h1>
   </div>
 </template>
 
@@ -8,14 +8,45 @@
 export default {
   name: 'HelloWorld',
   data () {
-    return {}
+    return {
+      time: ''
+    }
+  },
+  methods: {
+    getTime (date) {
+      var Year = date.getFullYear()
+      var Month = date.getMonth() + 1
+      var Dates = date.getDate()
+      var week = date.getDay()
+      let h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      let m = date.getMinutes()
+      m = m < 10 ? ('0' + m) : m
+      let s = date.getSeconds()
+      s = s < 10 ? ('0' + s) : s
+      var weeks = ['日', '一', '二', '三', '四', '五', '六']
+      var getWeek = '星期' + weeks[week]
+      this.time = Year + '年' + Month + '月' + Dates + '日' + getWeek + ' ' + h + ':' + m + ':' + s
+    },
+    nowTimes () {
+      this.getTime(new Date())
+      setInterval(this.nowTimes, 1000)
+    }
+
+  },
+  created () {
+    this.nowTimes()
+  },
+  mounted () {
+    this.nowTimes()
   }
+
 }
 </script>
 
 <style scoped>
 h1 {
   font-weight: normal;
-  padding: 20% 0 0 40%;
+  padding: 20px;
 }
 </style>
