7f8f9bd3993b523ccae7734496a0463d23c7154d
Leong <980159737@qq.com>
2019-10-30 18:04:16 +0800

    clazz

---
+++ backend/partymanager/src/main/java/com/wyu/partymanager/mapper/ClazzMapper.java
@@ -0,0 +1,7 @@
+package com.wyu.partymanager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wyu.partymanager.entity.sys.Clazz;
+
+public interface ClazzMapper extends BaseMapper<Clazz> {
+}
---
+++ backend/partymanager/src/main/java/com/wyu/partymanager/servicedao/ClazzServiceDao.java
@@ -0,0 +1,7 @@
+package com.wyu.partymanager.servicedao;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wyu.partymanager.entity.sys.Clazz;
+
+public interface ClazzServiceDao extends IService<Clazz> {
+}
