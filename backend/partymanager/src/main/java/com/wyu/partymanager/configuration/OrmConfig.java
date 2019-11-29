267a7a3ac9808ccbf8ef2434edd88c1b22e8b3df
Leong <980159737@qq.com>
2019-11-29 16:21:46 +0800

    debug

--- backend/partymanager/src/main/java/com/wyu/partymanager/configuration/OrmConfig.java
+++ backend/partymanager/src/main/java/com/wyu/partymanager/configuration/OrmConfig.java
@@ -1,16 +1,14 @@
 package com.wyu.partymanager.configuration;
 
-import core.CachedDbConnection;
+import core.DefaultDbFactory;
 import core.inerface.IDbConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 import util.Model;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 /**
  * @author Leong
@@ -28,13 +26,8 @@ public class OrmConfig {
         config.Configuration configuration = new config.Configuration();
         configuration.setModel(Model.POOL_MODEL);
         configuration.setEnableCache(false);
-        JedisPool jedisPool = new JedisPool("localhost",6379);
-         IDbConnection dbConnection = null;
-        try {
-             dbConnection = new CachedDbConnection(dataSource,configuration,jedisPool);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return dbConnection;
+        configuration.setDataSource(dataSource);
+        configuration.setJedisPool(new JedisPool("localhost",6379));
+        return DefaultDbFactory.getDb(configuration);
     }
 }
